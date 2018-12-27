ee89eff297946dc5c99662abc338e04095391585
Kyle Houghton <kylebenz@hotmail.com>
2018-12-27 21:07:19 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -3,7 +3,7 @@ def my_select(collection)
   i = 0
   return_value = Array.new
   while i < collection.length
-    
+    return_value << collection[i] if yield(collection[i]) == true
     i += 1
   end
   return_value
