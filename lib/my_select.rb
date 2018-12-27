87acd82f3f396dfd60285973f0ad963eb171fd18
Kyle Houghton <kylebenz@hotmail.com>
2018-12-27 21:09:07 +0000

    All tests pass.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,10 @@
+
 def my_select(collection)
- # your code here!
+  i = 0
+  return_value = Array.new
+  while i < collection.length
+    return_value << collection[i] if yield(collection[i]) == true
+    i += 1
+  end
+  return_value
 end
